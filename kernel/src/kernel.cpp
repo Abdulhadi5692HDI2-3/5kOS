ca724c379f910f3cf3c81c9544f051b0738d6deb
Abdulhadi5692HDI2-3 <144045411+Abdulhadi5692HDI2-3@users.noreply.github.com>
2024-10-13 17:28:33 +0100

    cleanup

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -35,24 +35,7 @@ void KeStartup(BootParams LoaderParams) {
     uint64_t KernelSize = (uint64_t)&_KernelEnd - (uint64_t)&_KernelStart;
     uint64_t KernelPages = (uint64_t)KernelSize / 4096 + 1;
     Allocator.LockPages(&_KernelStart, KernelPages);
-    DisplayInterface.Print(RGB(255, 255, 255), "Total RAM: ");
-    DisplayInterface.Print(RGB(107, 28, 217), ToString(Allocator.GetTotalRAM() / 1024));
-    DisplayInterface.Print(RGB(255, 255, 255), " KB, Free RAM: ");
-    DisplayInterface.Print(RGB(107, 28, 217), ToString(Allocator.GetFreeRAM() / 1024));
-    DisplayInterface.Print(RGB(255, 255,  255), " KB, Used RAM: ");
-    DisplayInterface.Print(RGB(107, 28, 217), ToString(Allocator.GetUsedRAM() / 1024));
-    DisplayInterface.Print(RGB(255, 255,  255), " KB, Reserved RAM: ");
-    DisplayInterface.Print(RGB(107, 28, 217), ToString(Allocator.GetReservedRAM() / 1024));
-    DisplayInterface.Print(RGB(255, 255, 255), " KB                         \n"); // new lines create this weird | character tho it still does a newline
-    for (UINT t = 0; t < 20; t++) {
-        // we're gonna to 20 pages! for science
-        void* Addr = Allocator.RequestPage();
-        DisplayInterface.Print(RGB(255, 255, 255), "Page no. ");
-        DisplayInterface.Print(RGB(255, 255, 255), ToString(t));
-        DisplayInterface.Print(RGB(255, 255, 255), "  Address: ");
-        DisplayInterface.Print(RGB(255, 255, 255), ToHexString((uint64_t)Addr));
-        DisplayInterface.g_curpos = {0, DisplayInterface.g_curpos.y + 16};
-    }
+    
 }
 extern "C" void _start(BootParams BootParameters) {
     KeStartup(BootParameters);
